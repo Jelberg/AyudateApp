7d861ce0bb0bb22d5fbe5b3f2f9ae9e6a5c95786
Jessica Elberg <elbergjessica@gmail.com>
2017-08-18 15:59:57 -0400

    Perfil de usuario

--- Ayudate/app/src/main/java/com/example/elberg/ayudate/PerfilActivity.java
+++ Ayudate/app/src/main/java/com/example/elberg/ayudate/PerfilActivity.java
@@ -1,7 +1,19 @@
 package com.example.elberg.ayudate;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ImageView;
+
+import static android.R.attr.src;
 
 public class PerfilActivity extends AppCompatActivity {
 
@@ -9,5 +21,21 @@ public class PerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        setToolbar();// Añadir action bar
+        if (getSupportActionBar() != null) // Habilitar up button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setToolbar() {
+        // Añadir la Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_perfil);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
     }
 }
